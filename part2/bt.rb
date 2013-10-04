fe8e2c44de365690c6c0c4162aab0519f568b40f
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2013-10-04 19:58:47 +1000

    Experiment runs, but tracker doesn't start

--- part2/bt.rb
+++ part2/bt.rb
@@ -1,7 +1,7 @@
-defProperty('slice', '', "slice name")
+defProperty('slice', 'cdw', "slice name")
 defProperty('tracker', "1-#{property.slice}", "ID of tracker node")
-defProperty('leecher_player', "2-#{property.slice}, 3-#{property.slice}", "List of leecher/player nodes")
-defProperty('seeder', "1-#{property.slice}, 4-#{property.slice}, 5-#{property.slice}", "List of seeder nodes")
+defProperty('leecher_player', ["2-#{property.slice}", "3-#{property.slice}"], "List of leecher/player nodes")
+defProperty('seeder', ["1-#{property.slice}", "4-#{property.slice}", "5-#{property.slice}"], "List of seeder nodes")
 
 defApplication('bttrack') do |app|
   app.description = 'Bittornado BT tracker'
@@ -33,9 +33,9 @@ end
 defApplication('vlc') do |app|
   app.description = 'VideoLAN client media player'
   app.binary_path = '/usr/local/bin/vlc.sh'
-  app.defProperty('stream', 'URL to play stream from', nil, {:type => :string})
-
-  app.defMeasurement("vlc_video") do |mp|
+  # property name 'stream' causes Openfire to kick us...
+  app.defProperty('xstream', 'URL to play stream from', nil, {:type => :string})
+  app.defMeasurement("video") do |mp|
     mp.defMetric('i_decoded_video_blocks', :int32)
     mp.defMetric('i_played_video_frames', :int32)
     mp.defMetric('i_lost_video_frames', :int32)
@@ -44,35 +44,47 @@ end
 
 defGroup('tracker', property.tracker) do |g|
   g.addApplication("bttrack") do |app|
-    r = Random.new
-    app.setProperty('dfile', "/tmp/dfile#{r.rand(10}")
+    app.setProperty('dfile', "/tmp/dfile_#{Time.now.to_i}")
     app.setProperty('port', 6969)
   end
 end
 
-defGroup('seeder', property.seeder) do |g|
+defGroup('seeder', *property.seeder.value) do |g|
   g.addApplication("transmission-daemon") do |app|
     app.setProperty('foreground', true)
+    app.measure('stats', :samples => 1)
   end
 end
 
-defGroup('leecher-player', property.leecher_player) do |g|
-  g.exec("rm /root/Downloads/*")
-  # TODO also remove any incomplete / pending downloads here
+defGroup('leecher_player', *property.leecher_player.value) do |g|
   g.addApplication("transmission-daemon") do |app|
     app.setProperty('foreground', true)
-    app.measure('stats')
+    app.measure('stats', :samples => 1)
   end
   g.addApplication("vlc") do |app|
-    app.setProperty('stream', "http://localhost:8080/big_buck_bunny_480p_stereo.avi?tor_url=http://192.168.1.1/big_buck_bunny_480p_stereo.avi.torrent")
-    app.measure('vlc_video')
+   app.setProperty('xstream', "http://localhost:8080/big_buck_bunny_480p_stereo.avi?tor_url=http://192.168.1.1/big_buck_bunny_480p_stereo.avi.torrent")
+   app.measure('video', :samples => 1)
   end
 end
 
-onEvent(:ALL_UP) do |event|
+onEvent(:ALL_NODES_UP) do |event|
   info "Starting bittorrent streaming experiment"
-  allGroups.startApplications
-  wait 60
-  allGroups.stopApplications
-  Experiment.done
+#  allGroups.exec("killall -s9 transmission-daemon; killall -s9 bttrack; killall -s9 vlc; rm /root/.config/transmission-daemon/settings.json")
+#  group("leecher_player").exec("rm /root/Downloads/* /root/.config/transmission-daemon/settings.json /root/.config/transmission-daemon/resume/*")
+  after 20 do
+    info "Starting tracker"
+    group("tracker").startApplications
+  end
+  after 25 do
+    info "Starting seeders"
+    group("seeder").startApplications
+  end
+  after 30 do
+    info "Starting players"
+    group("leecher_player").startApplications
+  end
+  after 120 do
+    allGroups.stopApplications
+    Experiment.done
+  end
 end

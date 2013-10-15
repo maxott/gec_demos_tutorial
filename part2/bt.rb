70e3c3a600ebbb2aa971770f67240183605d6c17
Christoph Dwertmann <christoph.dwertmann@nicta.com.au>
2013-10-15 21:04:02 +1100

    First working version of part2

--- part2/bt.rb
+++ part2/bt.rb
@@ -1,7 +1,11 @@
 defProperty('slice', 'cdw', "slice name")
-defProperty('tracker', "1-#{property.slice}", "ID of tracker node")
-defProperty('leecher_player', ["2-#{property.slice}", "3-#{property.slice}"], "List of leecher/player nodes")
-defProperty('seeder', ["1-#{property.slice}", "4-#{property.slice}", "5-#{property.slice}"], "List of seeder nodes")
+defProperty('tracker', "1", "ID of tracker node")
+defProperty('leecher_player', "2,3", "List of leecher/player nodes")
+defProperty('seeder', "4,5", "List of seeder nodes")
+
+tracker = "#{property.tracker.value}-#{property.slice.value}"
+leecher_player = property.leecher_player.value.split(',').map { |x| "#{x}-#{property.slice.value}" }
+seeder = property.seeder.value.split(',').map { |x| "#{x}-#{property.slice.value}" }
 
 defApplication('bttrack') do |app|
   app.description = 'Bittornado BT tracker'
@@ -10,9 +14,9 @@ defApplication('bttrack') do |app|
   app.defProperty('port', 'Port number', '--port', {:type => :integer})  
 end
 
-defApplication('transmission-daemon') do |app|
+defApplication('transmission_daemon') do |app|
   app.description = 'bittorrent client with video streaming support'
-  app.binary_path = '/usr/local/bin/transmission-daemon'
+  app.binary_path = 'killall -s9 transmission-daemon; rm /root/.config/transmission-daemon/settings.json; /usr/local/bin/transmission-daemon'
   app.defProperty('foreground', 'Run in foreground', '-f', {:type => :boolean})
 
   app.defMeasurement("stats") do |mp|
@@ -24,15 +28,15 @@ defApplication('transmission-daemon') do |app|
   end
 end
 
-defApplication('transmission-remote') do |app|
-  app.description = 'remote controller for transmission-daemon'
-  app.binary_path = '/usr/local/bin/transmission-remote'
-  app.defProperty('uplimit', 'Upload limit', '-u', {:type => :integer})
-end
+# defApplication('transmission-remote') do |app|
+#   app.description = 'remote controller for transmission-daemon'
+#   app.binary_path = '/usr/local/bin/transmission-remote'
+#   app.defProperty('uplimit', 'Upload limit', '-u', {:type => :integer})
+# end
 
 defApplication('vlc') do |app|
   app.description = 'VideoLAN client media player'
-  app.binary_path = '/usr/local/bin/vlc.sh'
+  app.binary_path = 'killall -s9 vlc; /usr/local/bin/vlc.sh'
   # property name 'stream' causes Openfire to kick us...
   app.defProperty('xstream', 'URL to play stream from', nil, {:type => :string})
   app.defMeasurement("video") do |mp|
@@ -42,49 +46,73 @@ defApplication('vlc') do |app|
   end
 end
 
-defGroup('tracker', property.tracker) do |g|
+defGroup('tracker', *tracker) do |g|
   g.addApplication("bttrack") do |app|
     app.setProperty('dfile', "/tmp/dfile_#{Time.now.to_i}")
     app.setProperty('port', 6969)
   end
 end
 
-defGroup('seeder', *property.seeder.value) do |g|
-  g.addApplication("transmission-daemon") do |app|
+defGroup('seeder', *seeder) do |g|
+  g.addApplication("transmission_daemon") do |app|
     app.setProperty('foreground', true)
     app.measure('stats', :samples => 1)
   end
 end
 
-defGroup('leecher_player', *property.leecher_player.value) do |g|
-  g.addApplication("transmission-daemon") do |app|
+defGroup('leecher', *leecher_player) do |g|
+  g.addApplication("transmission_daemon") do |app|
     app.setProperty('foreground', true)
     app.measure('stats', :samples => 1)
   end
+end
+
+defGroup('player', *leecher_player) do |g|
   g.addApplication("vlc") do |app|
-   app.setProperty('xstream', "http://localhost:8080/big_buck_bunny_480p_stereo.avi?tor_url=http://192.168.1.1/big_buck_bunny_480p_stereo.avi.torrent")
-   app.measure('video', :samples => 1)
+    app.setProperty('xstream', "http://localhost:8080/big_buck_bunny_480p_stereo.avi?tor_url=http://192.168.1.1/big_buck_bunny_480p_stereo.avi.torrent")
+    app.measure('video', :samples => 1)
   end
 end
 
-onEvent(:ALL_NODES_UP) do |event|
-  info "Starting bittorrent streaming experiment"
-#  allGroups.exec("killall -s9 transmission-daemon; killall -s9 bttrack; killall -s9 vlc; rm /root/.config/transmission-daemon/settings.json")
-#  group("leecher_player").exec("rm /root/Downloads/* /root/.config/transmission-daemon/settings.json /root/.config/transmission-daemon/resume/*")
-  after 20 do
+onEvent(:ALL_UP_AND_INSTALLED) do |event|
+  group("leecher").exec("rm /root/Downloads/* /root/.config/transmission-daemon/resume/* /root/.config/transmission-daemon/torrents/*")
+  after 5 do
     info "Starting tracker"
     group("tracker").startApplications
   end
-  after 25 do
-    info "Starting seeders"
-    group("seeder").startApplications
+  after 10 do
+   info "Starting seeders"
+   group("seeder").startApplications
   end
-  after 30 do
+  after 10 do
+   info "Starting leechers"
+   group("leecher").startApplications
+  end
+  after 12 do
     info "Starting players"
-    group("leecher_player").startApplications
+    group("player").startApplications
   end
-  after 120 do
+  # big buck bunny playtime = 10m plus buffering
+  after 800 do
     allGroups.stopApplications
     Experiment.done
   end
+end
+
+defGraph 'Bittorrent' do |g|
+  g.ms('stats').select {[ :oml_sender_id, :oml_seq, :oml_ts_server, :percent_done ]}
+  g.caption "Bittorrent (torrent completion)"
+  g.type 'line_chart3'
+  g.mapping :x_axis => :oml_ts_server, :y_axis => :percent_done, :group_by => :oml_sender_id
+  g.xaxis :legend => 'time [s]'
+  g.yaxis :legend => 'Completion', :ticks => {:format => 's'}
+end
+
+defGraph 'VLC Player' do |h|
+  h.ms('video').select {[ :oml_sender_id, :oml_seq, :oml_ts_server, :i_played_video_frames ]}
+  h.caption "VLC Player (frames played)"
+  h.type 'line_chart3'
+  h.mapping :x_axis => :oml_ts_server, :y_axis => :i_played_video_frames, :group_by => :oml_sender_id
+  h.xaxis :legend => 'time [s]'
+  h.yaxis :legend => 'Frames played', :ticks => {:format => 's'}
 end
---
+++ topologies/postboot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+host=$self.Name()
+slice=$sliceName
+echo $host > /etc/hostname
+/bin/hostname -F /etc/hostname
+echo "---
+:uid: $host-$slice
+:uri: xmpp://<%= \"$host-$slice-#{Process.pid}\" %>:<%= \"$host-$slice-#{Process.pid}\" %>@221.199.209.231
+:environment: production
+:debug: false" > /etc/omf_rc/config.yml
+/etc/init.d/omf_rc restart

b07f34302874ca36782e02d86705f53739920fff
Christoph Dwertmann <cdwertmann@gmail.com>
2013-11-20 14:05:16 +1100

    use emmy9 as XMPP server in all topologies

--- tools/scan.rb
+++ tools/scan.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-for i in (1..9)
-  spawn "echo #{i}: `timeout -2 80 omf_ec -u xmpp://emmy9.casa.umass.edu exec test.rb -- --slice cdw_#{i} | grep ALL_NODES_UP` >> nodes.out"
+for i in (1..30)
+  spawn "echo #{i}: `timeout -2 80 omf_ec -u xmpp://emmy9.casa.umass.edu exec test.rb -- --slice oedl-#{i} | grep ALL_NODES_UP` >> nodes.out"
 end
 Process.waitall
 system "sort nodes.out -g; rm nodes.out"

aefd7d1d4c78484d4eb8dc3c7383fb41d11c029f
thierry <thierry.rakotoarivelo@nicta.com.au>
2014-10-09 23:28:28 +1100

    Use table to visualise ping and include Max's comments

--- gec21_oedl_tutorial/tutorial_2.rb
+++ gec21_oedl_tutorial/tutorial_2.rb
@@ -1,14 +1,17 @@
 # OEDL Script showing a user-defined event, which is triggered upon an  
 # experiment-generated measurements reaching a specific value
 #
-# - we have only 1 resources, which is running a sine signal generator at 2s period
+# - we have only 1 resources, which is running a sine signal generator at 2s 
+#   period
 # - the experiment monitors the sine values from that generator every 1s
-# - when the experiment detects that the last sine value is above an arbitrary 
-#   threshold of 0.9, then it instruct the resource to send one ICMP ping packet
-#   to a target host.
-# - both measurements from the sine generator and the ping packets are then showed
-#   on 2 plots, where we should see that a new ping RTT value appears only when
-#   the sine value has peaked above 0.9
+# - when the experiment detects that the absolute last sine value is above an 
+#   arbitrary threshold of 0.99, then it instruct the resource to send one ICMP 
+#   ping packet to a target host.
+# - both measurements from the sine generator and the ping packets are then
+#   showed on 2 figures, one is a graph that shows the sine values, the other
+#   is a table that shows the time at which an ICMP ping was sent and its RTT
+#   value. Thus the table's number of rows should be equal to the graph's number
+#   of positive and negative peaks.
 #
 loadOEDL('https://raw.githubusercontent.com/mytestbed/oml4r/master/omf/signalgen.rb')
 loadOEDL('https://raw.githubusercontent.com/mytestbed/oml4r/master/omf/ping-oml2.rb')
@@ -32,8 +35,9 @@ defGroup('Pinger', property.res1) do |group|
   end
 end
 
-# Note: remember that sampling frequency > 2 * frequency of observed phenomenon
-defEvent(:CHECK_MEASUREMENT, every: 1) do
+# Note: 
+# Remember that event checking frequency > 2 * frequency of observed phenomenon
+defEvent(:SINE_ABOVE_THRESHOLD, every: 1) do
 
   # Query for some measurements...
   # returns an array where each element is a hash representing a row from the DB
@@ -47,9 +51,9 @@ defEvent(:CHECK_MEASUREMENT, every: 1) do
   # data = defQuery('select oml_ts_client as ts, value from signalgen_sin')
 
   triggered = false
-  if !data.nil? && !(last_row = data.pop).nil?
-    next if peak_list.include?(last_row[:oml_ts_client])
-    if last_row[:value] > 0.90
+  if !data.nil? && !(last_row = data.pop).nil? # Make sure we have some data
+    next if peak_list.include?(last_row[:oml_ts_client]) # Do nothing if we have seen this sample before
+    if last_row[:value].abs > 0.99
       peak_list << last_row[:oml_ts_client] # record that sample, so we dont trigger on it again
       triggered = true
     end
@@ -57,19 +61,15 @@ defEvent(:CHECK_MEASUREMENT, every: 1) do
   triggered
 end
 
-onEvent(:CHECK_MEASUREMENT, consume_event = false) do
-  info "TEST - TRIGGERED - #{Time.now}"
+onEvent(:SINE_ABOVE_THRESHOLD, consume_event = false) do
   group('Pinger').startApplications
 end
 
 onEvent(:ALL_UP_AND_INSTALLED) do 
   info "Starting a remote signal generator"
   group('Generator').startApplications
-  info "All my Applications are started now..."
-
-  after 40 do
+  after 80 do
     group('Generator').stopApplications
-    info "All my Applications are stopped now."
     Experiment.done
   end
 end
@@ -84,11 +84,18 @@ defGraph 'Sine' do |g|
 end
 
 defGraph 'Ping' do |g|
-  g.ms('ping').select {[ oml_ts_client.as(:ts), :rtt ]}
-  g.caption "Ping only when Sine > 0.9"
-  g.type 'line_chart3'
-  g.mapping :x_axis => :ts, :y_axis => :rtt
-  g.xaxis :legend => 'time [s]'
-  g.yaxis :legend => 'Ping RTT [ms]', :ticks => {:format => 's'}
+  g.ms('ping').select {[ oml_ts_client.as(:time_s), rtt.as(:rtt_ms) ]}
+  g.caption "Ping when Sine's absolute value > 0.99"
+  g.type 'table2'
+  g.mapping :x_axis => :time_s, :y_axis => :rtt_ms
 end
+
+# defGraph 'Ping' do |g|
+#   g.ms('ping').select {[ oml_ts_client.as(:ts), :rtt ]}
+#   g.caption "Ping only when Sine > 0.9"
+#   g.type 'line_chart3'
+#   g.mapping :x_axis => :ts, :y_axis => :rtt
+#   g.xaxis :legend => 'time [s]'
+#   g.yaxis :legend => 'Ping RTT [ms]', :ticks => {:format => 's'}
+# end
 
